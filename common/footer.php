e96c7ac10da9565bc3d5092f7631d9e4ce166185
Joel Thomas <hello@joelcthomas.com>
2016-12-27 15:18:43 -0500

    Original version commit

---
+++ common/footer.php
@@ -0,0 +1,19 @@
+<?php
+// footer contents
+?>
+		<section id="footer">
+			<ul class="icons">
+				<li><a href="javascript:void(0);" class="fa fa-envelope-o solo" id="emailIcon"><span>Email</span></a></li>
+				<li><a href="https://www.linkedin.com/in/joelcthomas" target="_blank" class="fa fa-linkedin solo"><span>LinkedIn</span></a></li>
+				<li><a href="https://www.facebook.com/joelthomas" target="_blank" class="fa fa-facebook solo"><span>Facebook</span></a></li>
+				<li><a href="https://twitter.com/joelcthomas" target="_blank" class="fa fa-twitter solo"><span>Twitter</span></a></li>
+			</ul>
+			<div class="copyright">
+				<ul class="menu">
+					<li>&copy; 2007 - 2017 joelcthomas. All rights reserved.</li>
+				</ul>
+			</div>
+		</section>
+
+	</body>
+</html>
